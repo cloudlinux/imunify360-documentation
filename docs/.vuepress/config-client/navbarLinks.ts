8465a72f6ccfe9c6b23bb9a4a3c0e9eb925f2101
Bogdan.ssh <bshyshka@gmail.com>
2023-06-26 16:22:29 +0300

    Add files via upload

---
+++ docs/.vuepress/config-client/navbarLinks.ts
@@ -0,0 +1,20 @@
+export default [
+    {
+      text: "Ask GPT?",
+      event: {
+        type: 'event',
+        name: 'botui-open'
+      },
+      class: "btn"
+    },
+    {
+      text: "Submit support request",
+      url: "https://www.cloudlinux.com/support-portal/",
+      class: "btn"
+    },
+    {
+      text: "Try Free",
+      url: "https://cloudlinux.com/trial",
+      class: "btn btn-white"
+    }
+  ]
