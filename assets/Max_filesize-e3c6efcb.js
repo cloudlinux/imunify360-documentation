a4f960ebacaf3a642e43c7c9deb951595533e792
annlev <71260515+annlev@users.noreply.github.com>
2023-11-29 19:13:06 +0000

    Deploying to gh-pages from  @ 4d1e2297cbc641483c5bb424ebaba77a2f357950 🚀

---
+++ assets/Max_filesize-e3c6efcb.js
@@ -0,0 +1,1 @@
+const s="/images/Max_filesize.png";export{s as _};
---
+++ assets/index.html-22cab04e.js
@@ -0,0 +1,1 @@
+const a=JSON.parse('{"key":"v-4033d0f8","path":"/patchman/","title":"Patchman","lang":"en-US","frontmatter":{},"headers":[]}');export{a as data};
